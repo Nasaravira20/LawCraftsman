f3e841e1f1b706cfed5a762acc15a3f7425f10c7
Nasaravira20 <arivuofficialnasa@gmail.com>
2024-10-05 21:27:01 +0530

    backend ingestion stated

---
+++ Ingestion/ingest.py
@@ -0,0 +1,38 @@
+import os
+import chromadb
+from chromadb.config import Settings
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+persist_directory = '/home/mw/Documents/Projects/Law/Ingestion/data'
+chroma_client = chromadb.PersistentClient(path=persist_directory)
+
+embedding_function = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+folder_path = "/home/mw/Documents/Projects/Law/Ingestion/source"
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+whole_chunked_documents = []
+if(os.path.exists(persist_directory)):
+    print(persist_directory)
+for filename in os.listdir(folder_path):
+    if filename.endswith(".pdf"):
+        file_path = os.path.join(folder_path, filename)
+        loader = PyPDFLoader(file_path=file_path)
+        document = loader.load()
+
+        chunked_documents = text_splitter.split_documents(document)
+        whole_chunked_documents.extend(chunked_documents)
+
+db = Chroma.from_documents(
+    documents=whole_chunked_documents,
+    embedding=embedding_function,
+    collection_name="law_documents",
+    persist_directory=persist_directory,
+    client=chroma_client,
+)
+
+
+
+print(f"Added {len(whole_chunked_documents)} chunks to Chroma DB")
---
+++ testing.py
@@ -0,0 +1,40 @@
+from huggingface_hub import InferenceClient
+
+import json
+
+
+repo_id = "Qwen/Qwen2-VL-7B-Instruct"
+
+
+llm_client = InferenceClient(
+
+    model=repo_id,
+
+    timeout=120,
+
+)
+
+
+def call_llm(inference_client: InferenceClient, prompt: str):
+
+    response = inference_client.post(
+
+        json={
+
+            "inputs": prompt,
+
+            "parameters": {"max_new_tokens": 200},
+
+            "task": "text-generation",
+
+        },
+
+    )
+
+    return json.loads(response.decode())[0]["generated_text"]
+
+
+
+response=call_llm(llm_client, "Laws in india")
+
+print (response)
